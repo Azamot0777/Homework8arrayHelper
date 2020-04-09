69da9e5e0390fa8548c45e2a74be328d6126191e
Heisenberg <Persey_98@list.ru>
2020-04-09 09:06:18 +0500

    created method pop

--- Program.cs
+++ Program.cs
@@ -6,16 +6,46 @@ namespace Homework8_ArrayHelper
     {
         static void Main(string[] args)
         {
-
+            // int 
+            int[] array1 = { 11, 22, 33, 44, 55, 66 };
+            foreach (int i in array1) Console.WriteLine(i);
         }
 
     }
 
 
-    class ArrayHelper
+    static class ArrayHelper
     {
-
-
+        public static int Pop(ref int[] array)
+        {
+            int n = array[array.Length - 1];
+            Array.Resize(ref array, array.Length - 1);
+            return n;
+        }
+        public static string Pop(ref string[] array)
+        {
+            string n = array[array.Length - 1];
+            Array.Resize(ref array, array.Length - 1);
+            return n;
+        }
+        public static double Pop(ref double[] array)
+        {
+            double n = array[array.Length - 1];
+            Array.Resize(ref array, array.Length - 1);
+            return n;
+        }
+        public static decimal Pop(ref decimal[] array)
+        {
+            decimal n = array[array.Length - 1];
+            Array.Resize(ref array, array.Length - 1);
+            return n;
+        }
+        public static float Pop(ref float[] array)
+        {
+            float n = array[array.Length - 1];
+            Array.Resize(ref array, array.Length - 1);
+            return n;
+        }
     }
 
 }
